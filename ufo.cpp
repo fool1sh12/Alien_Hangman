c54358d3d30804685a16769a29981ff43eaa384d
Georgio Khachadourian <110858737+fool1sh12@users.noreply.github.com>
2023-02-14 10:43:15 -0800

    My first commit
    Commit containing files for UFO game.

---
+++ ufo.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+  
+  greet();
+
+  std::string codeword = "scrumptious";
+  std::string answer = "___________";
+  int misses = 0;
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+
+  while (answer != codeword && misses < 7) {
+
+    display_misses(misses);
+    display_status(incorrect, answer);
+
+    std::cout << "\n\nPlease enter your guess: ";
+    std::cin >> letter;
+
+    for (int i = 0; i < codeword.length(); i++) {
+
+      if (letter == codeword[i]) {
+
+        answer[i] = letter;
+        guess = true;
+
+      }
+    
+    }
+
+    if (guess) {
+      std::cout << "Correct!\n";
+    }
+    else {
+      std::cout << "Incorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+
+  guess = false;
+
+  }
+
+  end_game(answer, codeword);
+  
+}
